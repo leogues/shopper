697f830a1f803bec0dad33188953efd6145c9c67
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2024-08-28 11:20:02 -0300

    chore: add postgres database and typeorm

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import dataSource from './database/typeorm/datasource'
 import { errorMiddleware } from './middleware/errorMiddleware'
 import routes from './routes'
 
 const app = express()
 
+dataSource.driver.connect().then(() => {
+  console.log('Connected to database')
+})
 app.use(express.json({ limit: '10mb' }))
 app.use(routes)
 app.use(errorMiddleware)
